eb7029fde6481dcaa7c0e311b40d9c3baeb4d4bb
A-WOLF-LION-BEAR <sorokin.as@phystech.edu>
2024-11-20 00:32:30 +0300

    Update task-B.cpp

--- contest1/task-B.cpp
+++ contest1/task-B.cpp
@@ -1,29 +1,29 @@
-// 119761345
+// 125684331
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
-const long long cInf = 10004321;
-const long long cN45 = 45;
-const long long cN123 = 123;
+const int64_t cInf = 10004321;
+const int64_t cN45 = 45;
+const int64_t cN123 = 123;
 
-long long FindPivot(std::vector<long long> mas) {
-  long long answer;
+int64_t FindPivot(std::vector<int64_t> mas) {
+  int64_t answer;
   if (int(mas.size()) <= 5) {
     std::sort(mas.begin(), mas.end());
     answer = mas[int(mas.size()) / 2];
   } else {
-    long long size = int(mas.size()) / 5;
-    std::vector<long long> delen(size);
-    for (long long i = 0; i < size; i++) {
-      std::vector<long long> a = {mas[5 * i], mas[5 * i + 1], mas[5 * i + 2],
-                                  mas[5 * i + 3], mas[5 * i + 4]};
+    int64_t size = int(mas.size()) / 5;
+    std::vector<int64_t> delen(size);
+    for (int64_t i = 0; i < size; i++) {
+      std::vector<int64_t> a = {mas[5 * i], mas[5 * i + 1], mas[5 * i + 2],
+                                mas[5 * i + 3], mas[5 * i + 4]};
       std::sort(a.begin(), a.end());
       delen[i] = a[int(a.size()) / 2];
     }
     if (5 * size < int(mas.size())) {
-      std::vector<long long> ostat(mas.end() - (int(mas.size()) - (5 * size)),
-                                   mas.end());
+      std::vector<int64_t> ostat(mas.end() - (int(mas.size()) - (5 * size)),
+                                 mas.end());
       std::sort(ostat.begin(), ostat.end());
       delen.push_back(ostat[int(ostat.size()) / 2]);
     }
@@ -32,88 +32,88 @@ long long FindPivot(std::vector<long long> mas) {
   return answer;
 }
 
-std::vector<long long> CreateMassiv(long long n, long long a0, long long a1) {
-  std::vector<long long> chisla(n);
-  chisla[0] = a0;
-  chisla[1] = a1;
-  for (long long i = 2; i < n; i++) {
-    if (((chisla[i - 1] * cN123) + (chisla[i - 2] * cN45)) >= cInf) {
-      long long per = ((chisla[i - 1] * cN123) + (chisla[i - 2] * cN45));
+std::vector<int64_t> Createarray(int64_t n, int64_t a0, int64_t a1) {
+  std::vector<int64_t> nums(n);
+  nums[0] = a0;
+  nums[1] = a1;
+  for (int64_t i = 2; i < n; i++) {
+    if (((nums[i - 1] * cN123) + (nums[i - 2] * cN45)) >= cInf) {
+      int64_t per = ((nums[i - 1] * cN123) + (nums[i - 2] * cN45));
       while (per >= cInf) {
         per -= cInf;
       }
-      chisla[i] = per;
+      nums[i] = per;
     } else {
-      chisla[i] = ((chisla[i - 1] * cN123) + (chisla[i - 2] * cN45));
+      nums[i] = ((nums[i - 1] * cN123) + (nums[i - 2] * cN45));
     }
   }
-  return chisla;
+  return nums;
 }
 
-std::vector<long long> Partition(const std::vector<long long>& massiv,
-                                 long long pivot) {
-  long long b = 0;
-  long long c = 0;
-  long long d = 0;
-  for (long long i = 0; i < int(massiv.size()); i++) {
-    if (massiv[i] < pivot) {
+std::vector<int64_t> Partition(const std::vector<int64_t>& array,
+                               int64_t pivot) {
+  int64_t b = 0;
+  int64_t c = 0;
+  int64_t d = 0;
+  for (int64_t i = 0; i < int(array.size()); i++) {
+    if (array[i] < pivot) {
       b++;
-    } else if (massiv[i] == pivot) {
+    } else if (array[i] == pivot) {
       c++;
     } else {
       d++;
     }
   }
-  std::vector<long long> bcd = {b, c, d};
+  std::vector<int64_t> bcd = {b, c, d};
   return bcd;
 }
 
-std::vector<long long> QSArray(const std::vector<long long>& massiv,
-                               long long pivot, int n) {
-  std::vector<long long> d;
+std::vector<int64_t> QSArray(const std::vector<int64_t>& array, int64_t pivot,
+                             int n) {
+  std::vector<int64_t> d;
   if (n == 1) {
-    for (long long i = 0; i < int(massiv.size()); i++) {
-      if (massiv[i] < pivot) {
-        d.push_back(massiv[i]);
+    for (int64_t i = 0; i < int(array.size()); i++) {
+      if (array[i] < pivot) {
+        d.push_back(array[i]);
       }
     }
   } else {
-    for (long long i = 0; i < int(massiv.size()); i++) {
-      if (massiv[i] > pivot) {
-        d.push_back(massiv[i]);
+    for (int64_t i = 0; i < int(array.size()); i++) {
+      if (array[i] > pivot) {
+        d.push_back(array[i]);
       }
     }
   }
   return d;
 }
 
-long long QuikeSelekt(const std::vector<long long>& massiv, long long neednum) {
-  long long answer = 0;
-  if (massiv.size() <= 1) {
-    answer = massiv[0];
+int64_t QuikeSelekt(const std::vector<int64_t>& array, int64_t neednum) {
+  int64_t answer = 0;
+  if (array.size() <= 1) {
+    answer = array[0];
   } else {
-    long long pivot = FindPivot(massiv);
-    std::vector<long long> index = Partition(massiv, pivot);
-    long long l = index[0];
-    long long m = index[1];
+    int64_t pivot = FindPivot(array);
+    std::vector<int64_t> index = Partition(array, pivot);
+    int64_t l = index[0];
+    int64_t m = index[1];
     if (l <= neednum && neednum < l + m) {
       answer = pivot;
     } else if (neednum < l) {
-      answer = QuikeSelekt(QSArray(massiv, pivot, 1), neednum);
+      answer = QuikeSelekt(QSArray(array, pivot, 1), neednum);
     } else if (neednum >= m + l) {
-      answer = QuikeSelekt(QSArray(massiv, pivot, 0), neednum - l - m);
+      answer = QuikeSelekt(QSArray(array, pivot, 0), neednum - l - m);
     }
   }
   return answer;
 }
 
-int main() {
-  long long n;
-  long long k;
-  long long a0;
-  long long a1;
+int mainTaskB() {
+  int64_t n;
+  int64_t k;
+  int64_t a0;
+  int64_t a1;
   std::cin >> n >> k >> a0 >> a1;
   k--;
-  std::vector<long long> numbers = CreateMassiv(n, a0, a1);
+  std::vector<int64_t> numbers = Createarray(n, a0, a1);
   std::cout << QuikeSelekt(numbers, k) << "\n";
 }
